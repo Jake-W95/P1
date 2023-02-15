1d07f42b632662ec1cc57eef9ce1b6c00e03f8ca
PaulKnees <jakewallace368@gmail.com>
2023-02-15 20:51:12 +0000

    small bug, often works very well

--- round.js
+++ round.js
@@ -4,7 +4,10 @@ function round(hand1, hand2) {
     var i = 0;
     var j = 0;
     function play() {
-        console.log('i = ' + i,'j = ' + j)
+        if (hand1 === 0 || hand2 === 0) {
+            return console.log('Game Over')
+        }
+        console.log('i = ' + i, 'j = ' + j)
         const roundU = {
             name: 'round',
             message: hand1[i].name,
@@ -24,24 +27,28 @@ function round(hand1, hand2) {
         ]);
 
         function wonRound() {
-            hand1.splice(i, 0, hand2.splice(j, 1))
+            hand1.splice(i--, 0, hand2.splice(j, 1))
             console.log('You win this card', 'hand1 ' + hand1, 'hand2 ' + hand2)
             nextRound()
         }
         function lostRound() {
-            hand2.splice(j,0,hand1.splice(i, 1))
+            hand2.splice(j--, 0, hand1.splice(i, 1))
             console.log('You lose this card', 'hand1 ' + hand1, 'hand2 ' + hand2)
             nextRound()
         }
         function nextRound() {
-            if (i === hand1.length-1) {
+            if (i === hand1.length - 1) {
                 i = 0
+            } else if (hand1.length == 0) {
+                return console.log('You Lost')
             } else {
                 i++
             }
-            if (j === hand2.length-1) {
+            if (j === hand2.length - 1) {
                 j = 0
-            } else {
+            } else if(hand2.length === 0){
+                return console.log('You Won')
+            } {
                 j++
             }
             // i === 
@@ -66,9 +73,10 @@ function round(hand1, hand2) {
 
                 })
 
-        } else {
-            console.log('Game Over')
         }
+        // else {
+        //     console.log('Game Over')
+        // }
 
     }
 
