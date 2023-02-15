7cf135c8cdae5b74192c59a29135cc4227916c4c
PaulKnees <jakewallace368@gmail.com>
2023-02-15 15:31:06 +0000

    round logic started

--- gameLogic.js
+++ gameLogic.js
@@ -1,5 +1,8 @@
+const inquirer = require('inquirer');
+
 const shuffle = require('./shuffleFunc');
-const deal = require('./dealFunc')
+const deal = require('./dealFunc');
+const round = require('./round');
 
 class Card {
     constructor(name, strength, speed, height, weight) {
@@ -17,23 +20,20 @@ let ch3 = new Card('Simone', 150, 80, 60, 200);
 let ch4 = new Card('Marcus', 200, 105, 82, 30);
 
 
-
-
-
 function startGame() {
     const deck = [ch1, ch2, ch3, ch4];
             // Results in 2 hands, shuffled
-    const hands = deal(shuffle(deck))
+    const hands = deal(shuffle(deck));
 
     const hand1 = hands[0]
     const hand2 = hands[1]
+    round(hand1, hand2)
 
     
 }
 
 
 startGame()
-
 
 
 
--- round.js
+++ round.js
@@ -1,6 +1,24 @@
 
 
-function round (){
+function round (hand1, hand2) {
+    const roundQ = {
+        name: 'round',
+        message: 'Select stat',
+        type: 'list',
+        choices: ['name', 'strength', 'speed', 'height', 'weight']
+    }
 
+    console.log(hand1)
+    if (hand1.length > 0 && hand2.length > 0){
 
+        console.log(hand1[0])
+        // inquirer.prompt(roundQ)
+        // .then((res) => {
+        
+        //     console.log(res)
+        
+        // })
+    }
 }
+
+module.exports = round
