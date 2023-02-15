3e7e0d113ef5a5200fe6648bca60c502ffbbb193
PaulKnees <jakewallace368@gmail.com>
2023-02-15 20:05:08 +0000

    IT LOOPS!

--- gameLogic.js
+++ gameLogic.js
@@ -12,25 +12,28 @@ class Card {
         this.weight = weight;
 
     }
-    
+
 }
 
 let ch1 = new Card('Steve', 100, 48, 180, 100);
 let ch2 = new Card('Lucy', 91, 60, 185, 69);
 let ch3 = new Card('Simone', 150, 80, 60, 200);
 let ch4 = new Card('Marcus', 200, 105, 82, 30);
+let ch5 = new Card('Thomas', 60, 140, 95, 90);
+let ch6 = new Card('April', 173, 190, 120, 140);
+let ch7 = new Card('Alice', 74, 200, 52, 145);
 
 
 function startGame() {
-    const deck = [ch1, ch2, ch3, ch4];
-            // Results in 2 hands, shuffled
+    const deck = [ch1, ch2, ch3, ch4, ch5, ch6, ch7];
+    // Results in 2 hands, shuffled
     const hands = deal(shuffle(deck));
 
     const hand1 = hands[0]
     const hand2 = hands[1]
     round(hand1, hand2)
 
-    
+
 }
 
 
--- round.js
+++ round.js
@@ -1,44 +1,77 @@
 const inquirer = require('inquirer');
 
 function round(hand1, hand2) {
-    const roundU = {
-        name: 'round',
-        message: hand1[0].name,
-        type: 'list',
-        choices: [
-            'Str: ' + hand1[0].strength,
-            'Spd: ' + hand1[0].speed,
-            'Hei: ' + hand1[0].height,
-            'Wei: ' + hand1[0].weight
-        ]
-    }
-    const roundAI = new Map([
-        ['Str', hand2[0].strength],
-        ['Spd', hand2[0].speed],
-        ['Hei', hand2[0].height],
-        ['Wei', hand2[0].weight]
-    ])
+    var i = 0;
+    var j = 0;
+    function play() {
+        console.log(i, j)
+        const roundU = {
+            name: 'round',
+            message: hand1[i].name,
+            type: 'list',
+            choices: [
+                'Str: ' + hand1[i].strength,
+                'Spd: ' + hand1[i].speed,
+                'Hei: ' + hand1[i].height,
+                'Wei: ' + hand1[i].weight
+            ]
+        };
+        const roundAI = new Map([
+            ['Str', hand2[j].strength],
+            ['Spd', hand2[j].speed],
+            ['Hei', hand2[j].height],
+            ['Wei', hand2[j].weight]
+        ]);
 
-    function lostRound () {
-        console.log('You lose this card')
+        function lostRound() {
+            console.log('You lose this card')
+            nextRound()
+        }
+        function wonRound() {
+            console.log('You win this card')
 
-    }
-    function wonRound () {
-        console.log('You win this card')
-    }
-    if (hand1.length > 0 && hand2.length > 0) {
-        inquirer.prompt(roundU)
-            .then((playerChoice) => {
-                const pAtt = playerChoice.round.slice(0, 3)
-                const pNum = parseInt(playerChoice.round.slice(5));
-                
-                const aiNum = roundAI.get(pAtt)
-                console.log(hand2[0].name + ' ' +pAtt + ':' + aiNum)
-                                                                        //Win/Lose state
-                pNum > aiNum ? wonRound() : lostRound()
+            nextRound()
+        }
+        function nextRound() {
+            if (i === hand1.length-1) {
+                i = 0
+            } else {
+                i++
+            }
+            if (j === hand2.length-1) {
+                j = 0
+            } else {
+                j++
+            }
+            // i === 
+            // j === 
+
+        }
+        if (hand1.length > 0 && hand2.length > 0) {
+
+
+            console.log(hand1.length, hand2.length)
+            inquirer.prompt(roundU)
+                .then((playerChoice) => {
+                    const pAtt = playerChoice.round.slice(0, 3)
+                    const pNum = parseInt(playerChoice.round.slice(5));
+
+                    const aiNum = roundAI.get(pAtt)
+                    console.log(hand2[j].name + ' ' + pAtt + ':' + aiNum)
+                    //Win/Lose state
+                    pNum > aiNum ? wonRound() : lostRound();
+                    // console.log(i, j)
+                    play()
+
                 })
-            } 
+
+        }
+
     }
+
+    play()
+
+}
 
 
 module.exports = round
