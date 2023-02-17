462f4d7674a2bdc678da0d26a7b6e7263d036061
Michael Yeates <michael-yeates@outlook.com>
2023-02-17 15:50:27 +0000

    Add news api/image api, with components on landing page

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,12 +1,17 @@
-const ArticleCard = ({ article, imageUrl }) => {
-  const { title } = article;
+const ArticleCard = ({ article, imageUrls }) => {
+  const { title, url } = article;
 
   return (
-    <div className="col-sm-12 col-md-12 col-lg-4">
-      <div className="article-card container">
-        {imageUrl && <img className="article-image" src={imageUrl} alt={title} />}
-        <h3 className="article-title">{title}</h3>
-      </div>
+    <div className="col-12 col-md-6 col-lg-6">
+      <a href={url}>
+        <div className="article-card">
+          {imageUrls && <img className="article-image" src={imageUrls} alt={title} />}
+          <div className="article-text">
+            <p className="article-news">POPULAR</p>
+            <h3 className="article-title">{title}</h3>
+          </div>
+        </div>
+      </a>
     </div>
   );
 };
