58e740565012f716b70d0975a8c4b0cda60c2028
PaulKnees <jakewallace368@gmail.com>
2023-02-17 21:10:52 +0000

    creates useState and () => for getting AIs attribute value, dependant on user att name

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -18,12 +18,18 @@ const Game = () => {
   const hands = shuffleDeal()
   const hand1 = hands[0];
   const hand2 = hands[1]
-
+                                // States Getting User's chosen Att Name and Value
   const [userAttVal, setUserAttVal] = useState(undefined)
   const [userAttName, setUserAttName] = useState(undefined)
 
+                                // 
   const getUserAttVal = (userAttVal) => {setUserAttVal(userAttVal)}
   const getUserAttName = (userAttName) => {setUserAttName(userAttName)}
+  
+                                // State Getting AI's Att Value for comparison
+  const [AIAttVal, setAIAttVal] = useState(undefined)
+  
+  const getAIVal = (AIAttVal) => {setAIAttVal(AIAttVal)} 
 
 
   return (
@@ -35,7 +41,7 @@ const Game = () => {
           <TrumpCard player="user" card={hand1[0]} getUserAttVal={getUserAttVal} getUserAttName={getUserAttName}/>
         </div>
         <div className="col-12 col-md-6">
-          <TrumpCard player="computer" card={hand2[0]} />
+          <TrumpCard player="computer" card={hand2[0]} userAttName={userAttName} getAIAttVal={getAIVal}/>
         </div>
       </div>
     </section>
