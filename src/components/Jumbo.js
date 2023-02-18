0d9e14bce0ff9e5f12cd1ead3d5b6085dcbb94b7
Michael Yeates <michael-yeates@outlook.com>
2023-02-18 17:29:22 +0000

    Add cars and animations to jumbo

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -7,7 +7,7 @@ const Jumbo = () => {
         <motion.h1
           initial={{ x: "-100vw" }}
           animate={{ x: "0" }}
-          transition={{ type: "spring", delay: 0.3, duration: 1, ease: "easeInOut" }}
+          transition={{ type: "spring", delay: 0.3, duration: 3, ease: "easeInOut" }}
         >
           P1
         </motion.h1>
@@ -15,11 +15,26 @@ const Jumbo = () => {
         <motion.p
           initial={{ x: "-100vw" }}
           animate={{ x: "0" }}
-          transition={{ type: "spring", bounce: "0.5", delay: 0.5, duration: 2, ease: [0.17, 0.67, 0.83, 0.67] }}
+          transition={{ type: "spring", bounce: "0.5", delay: 1, duration: 3.5, ease: [0.17, 0.67, 0.83, 0.67] }}
         >
-          {" "}
           Fuel your passion
         </motion.p>
+        <motion.img
+          className="car-one f1-car"
+          src="./images/car.svg"
+          alt="Red F1 Car"
+          initial={{ x: "-100vw" }}
+          animate={{ x: "100vw" }}
+          transition={{ type: "spring", duration: 8 }}
+        />
+        <motion.img
+          className="car-two f1-car"
+          src="./images/car2.svg"
+          alt="Silver F1 Car"
+          initial={{ x: "-100vw" }}
+          animate={{ x: "100vw" }}
+          transition={{ type: "spring", duration: 6 }}
+        />
       </div>
     </div>
   );
