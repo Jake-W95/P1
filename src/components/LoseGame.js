a90b2312e235b392a09d647161c672f9e1f254e4
Michael Yeates <michael-yeates@outlook.com>
2023-02-22 18:54:43 +0000

    Styling for outcome conditions

--- src/components/LoseGame.js
+++ src/components/LoseGame.js
@@ -1,13 +1,14 @@
-import { NavLink } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
 
 const LoseGame = () => {
-    return (
-        <section>
-            <h1>You Lost</h1>
-           <NavLink to='/'>Home</NavLink>
-        </section>
-    )
-}
+  return (
+    <section className="outcome-container">
+      <div className="outcome-wrap">
+        <h1>Don't worry, even Lewis Hamilton has bad days!</h1>
+        <NavLink to="/">Back to Home</NavLink>
+      </div>
+    </section>
+  );
+};
 
-export default LoseGame
+export default LoseGame;
--- src/components/WinGame.js
+++ src/components/WinGame.js
@@ -1,13 +1,14 @@
-import { NavLink } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
 
 const WinGame = () => {
-    return (
-        <section>
-            <h1>You Won!</h1>
-           <NavLink to='/'>Home</NavLink>
-        </section>
-    )
-}
+  return (
+    <section className="outcome-container">
+      <div className="outcome-wrap">
+        <h1>The checkered flag is yours - you've taken P1 in style!</h1>
+        <NavLink to="/">Back to Home</NavLink>
+      </div>
+    </section>
+  );
+};
 
-export default WinGame
+export default WinGame;
