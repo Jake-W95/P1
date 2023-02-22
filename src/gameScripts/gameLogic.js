ae617ebc9169520b1605681406cf2ef14518ae39
PaulKnees <jakewallace368@gmail.com>
2023-02-22 19:09:10 +0000

    replaces necessary Requires with Installs

--- src/gameScripts/gameLogic.js
+++ src/gameScripts/gameLogic.js
@@ -1,4 +1,5 @@
 // const round = require('./round');
+import deal from './dealFunc';
 
 ////////////////////////////////////////////////////////////// INITIALISE GAME
 function shuffleDeal() {
@@ -10,7 +11,7 @@ function shuffleDeal() {
     const shuffle = require('./shuffleFunc');
     
     // FUNCTION DEALING CARDS TO 2 HANDS
-    const deal = require('./dealFunc');
+    // const deal = require('./dealFunc');
     
     // GAMEPLAY
 
@@ -32,36 +33,3 @@ shuffleDeal()
 
 
 export default shuffleDeal
-
-
-
-
-
-// DRIVER CARDS CLASS
-
-// CLASS NOT NECESSARY, DRIVERS ARE ALREADY OBJECTS 
-// class Card {
-//     constructor(driverID, name, experience, teamRanking, wins, podiums, championships, rating, driverNum) {
-//         this.driverID = driverID /*API DRIVER NUMBER*/
-//         this.name = name /*NAME FOR DISPLAY */
-//                 // IN GAME ATTS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//         this.experience = experience                                                                 /*\\\\\\\\\\\\\\*/
-//         this.teamRanking = teamRanking                                                               /*\\\\\\\\\\\\\\*/
-//         this.wins = wins                                                                             /*\\\\\\\\\\\\\\*/
-//         this.podiums = podiums                                                                       /*\\\\\\\\\\\\\\*/
-//         this.championships = championships                                                           /*\\\\\\\\\\\\\\*/
-//         this.rating = rating                                                                         /*\\\\\\\\\\\\\\*/
-//                 // END OF IN GAME ATTS\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//         this.driverNum = driverNum /*DRIVER # FOR DISPLAY*/ 
-//     }
-// }
-
-// OLD DUMMY DATA
-// let ch1 = new Card('Steve', 100, 48, 180, 100);
-// let ch2 = new Card('Lucy', 91, 60, 185, 69);
-// let ch3 = new Card('Simone', 150, 80, 60, 200);
-// let ch4 = new Card('Marcus', 200, 105, 82, 30);
-// let ch5 = new Card('Thomas', 60, 140, 95, 90);
-// let ch6 = new Card('April', 173, 190, 120, 140);
-// let ch7 = new Card('Alice', 74, 200, 52, 145);
-// let ch8 = new Card('Callum', 210, 120, 140, 175);
