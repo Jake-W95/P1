d3d4de43c3a74bcf387e02a5d48ae9ba22fd9917
Michael Yeates <michael-yeates@outlook.com>
2023-02-22 16:36:58 +0000

    Add initial styling to game

--- src/components/GameInstructions.js
+++ src/components/GameInstructions.js
@@ -1,23 +1,48 @@
-
-
 const GameInstructions = ({ toggleInstructions, showInstructions }) => {
-    if (showInstructions) {
-        return (
-            <section className="game-instructions">
-                <h1>P1 Heroes</h1>
-                <h3>Welcome to P1 Heroes!</h3>
-                <h4>Pit your favourite Formula 1 drivers against each-other in this incredibly original card game that definitely isn't a rip-off of Top Trumps <br></br>
-                    Win cards by choosing an attribute that beats your opponents! But choose carefully, get it wrong and you'll lose your own card!<br></br>
-                    The winner is the player who ends up with all of the cards, the loser must hang their head in shame and think about what they've done. <br></br>
-                    Don't be a pathetic loser. Be a hero.</h4>
-                <h3 style={{marginTop: "2rem"}}>With Team Ranking the lowest number wins!</h3>
+  if (showInstructions) {
+    return (
+      <section className="game-instructions col-12">
+        <h3>Welcome to P1 Heroes!</h3>
 
-                <button onClick={toggleInstructions}>Toggle Show</button>
+        <div className="instructions-text-wrap">
+          <p>
+            Pit your favourite Formula 1 drivers against each-other in this incredibly original card game that
+            definitely isn't a rip-off of Top Trump
+          </p>
+          <p>
+            Win cards by choosing an attribute that beats your opponents! But choose carefully, get it wrong and you'll
+            lose your own card!
+          </p>
+          <p>
+            The winner is the player who ends up with all of the cards, the loser must hang their head in shame and
+            think about what they've done.{" "}
+          </p>
+          <p>Don't be a pathetic loser. Be a hero.</p>
+          <p>With Team Ranking the lowest number wins!</p>
+        </div>
 
-            </section>
-        )
-    }
-    return <button onClick={toggleInstructions}>Toggle Show</button>
-}
+        {/* <p>
+          Pit your favourite Formula 1 drivers against each-other in this incredibly original card game that definitely
+          isn't a rip-off of Top Trumps <br></br>
+          Win cards by choosing an attribute that beats your opponents! But choose carefully, get it wrong and you'll
+          lose your own card!<br></br>
+          The winner is the player who ends up with all of the cards, the loser must hang their head in shame and think
+          about what they've done. <br></br>
+          Don't be a pathetic loser. Be a hero.
+        </p>
+        <p>With Team Ranking the lowest number wins!</p> */}
 
-export default GameInstructions
+        <button className="instructions-btn hide-btn" onClick={toggleInstructions}>
+          Hide Instructions
+        </button>
+      </section>
+    );
+  }
+  return (
+    <button className="instructions-btn show-btn" onClick={toggleInstructions}>
+      Show Instructions
+    </button>
+  );
+};
+
+export default GameInstructions;
